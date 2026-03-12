deb55f518fac48682ab2fb36ea6c31fda9931e70
avandendool <38973634+fibonacci736@users.noreply.github.com>
2026-03-12 11:18:28 +0100

    model improved

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import Enum
 
 from event import Event,EventType
 from yard import ContainerYard, Container
@@ -8,31 +9,72 @@ class Scenario:
     events: list[Event]
     yard: ContainerYard
 
+@dataclass
+class MoveAction:
+    source : (int, int)
+    target : (int, int)
+
 class Agent:
-    def prepare_scenario(self,yard,events):
+    def start(self,yard,events):
         self.yard = yard
         self.events = events
 
-    def decide_where_to_put(self,container):
+    def notify(self,event: Event):
+        pass
+    def actions(self) -> list[MoveAction]:
+        return []
+
+class CoroAgent(Agent):
+    def coroutine(self):
+        pass
+    def notify(self,event: Event):
+        pass
+    def actions(self) -> list[MoveAction]:
         pass
 
+
 class Model:
-    def run(self,scenario : Scenario, agent):
+    EXTERNAL = (-1,-1)
+
+    def __init__(self,scenario,agent : Agent):
+        self.scenario = scenario
+        self.agent = agent
+        self.yard : ContainerYard = None
+
+    def run(self):
         time = 0
-        yard = scenario.yard.copy()
-        for event in scenario.events:
-            if event.type == EventType.ARRIVAL:
-                agent.
+        self.yard : ContainerYard = self.scenario.yard.copy()
+        events = self.scenario.events
+        self.agent.start(self.yard,events)
+        for event in events:
+            self.process_event(event)
+
+    def process_event(self,event:Event):
+        self.agent.notify(event)
+        done = False
+
+        for move in self.agent.actions():
+            if move.source == Model.EXTERNAL:
+                assert event.type == EventType.ARRIVAL,'there is no new container to be placed'
+                self.yard.put(event.subject, *move.target)
+                done = True
+            elif move.target == Model.EXTERNAL:
+                assert event.type == EventType.DEPARTURE, 'container shouldnt leave'
+                leaving_container = self.yard.pop(*move.source)
+                assert leaving_container == event.subject
+                done = True
+            else:
+                moving_container = self.yard.pop(*move.source)
+                self.yard.put(moving_container,*move.target)
+
+        assert done, "failed to place or remove container"
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
-    yard = ContainerYard()
-    for i in range(3):
-        yard.put(Container(), i, 0)
-    print(yard)
-    for i in range(5):
-        print(yard.can_pop(i, 0), yard.can_put(i, 0))
-    for i in range(2, -1, -1):
-        yard.pop(i, 0)
-    print(yard)
+    scenario = Scenario()
