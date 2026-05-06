fe9fa0f7b20a53457b379e9621c69b3cf06bf356
avandendool <38973634+fibonacci736@users.noreply.github.com>
2026-05-06 15:48:46 +0200

    manual scenarios for testing

--- model/testcases.py
+++ model/testcases.py
@@ -15,10 +15,7 @@ def random_scenario(yard_dim,N_containers,N_destinations):
     return Scenario(yard_dim,containers)
 random.seed(12345)
 small_scenarios = [
-    random_scenario((1,3),3,3),
-    random_scenario((1,9),9,2),
-    random_scenario((1,9),9,9),
-    random_scenario((2,3),6,2),
-    random_scenario((2,3),6,6),
-    random_scenario((2,4),8,2),
-                   ]
+    Scenario((1,9),[1,2,3,4,5,6,7,8,9]),
+    Scenario((2,9),[1,2,3,4,5,6,7,8,9]),
+    random_scenario((4,4),16,9)
+]
